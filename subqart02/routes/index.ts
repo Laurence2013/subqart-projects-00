23d9226d339158f2d7d76def4d018728257b7a78
Laurence2013 <lozza244@protonmail.com>
2025-02-21 14:32:54 +0000

    06-commit

--- subqart02/routes/index.ts
+++ subqart02/routes/index.ts
@@ -5,8 +5,8 @@ import sayHello from '../src/index.js'
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
-	const name: string = 'Craig Johnson';
-	const getName: any = sayHello(name);
+	//const name: string = 'Craig Johnson';
+	const getName: any = sayHello('');
 	res.json({msg: getName.name})
 });
 
--- subqart02/src/index.ts
+++ subqart02/src/index.ts
@@ -1,5 +1,15 @@
-function sayHello(fullName: string){
-	return {name: fullName};
+import { Map, List } from 'immutable';
+
+const user = Map({
+	name: 'John Doe',
+	age: 30,
+	hobbies: List(['Reading', 'Coding', 'Gaming'])
+});
+
+console.log(user.get('name'));
+
+function sayHello(fullName: string | null){
+	return {name: user.get('name')};
 };
 
 export default sayHello;
