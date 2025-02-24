3039207d644e6aa6bb1fce56dd44c5fc8f274db9
Laurence2013 <lozza244@protonmail.com>
2025-02-24 09:51:35 +0000

    11-commit

---
+++ subqart02/jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+	preset: 'ts-jest',
+  testEnvironment: 'node',
+	testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"], 
+	coverageDirectory: 'coverage', 
+	collectCoverage: true, 
+	moduleNameMapper: {
+		'^@/(.*)$': '<rootDir>/src/$1'
+	}
+};
+//export default config;
+module.exports = config;
