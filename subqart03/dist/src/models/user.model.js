d8ca37b5f41c7bd24b23c1d33ef65a75d65f42de
Laurence2013 <lozza244@protonmail.com>
2025-02-25 11:29:27 +0000

    00-remove-commit

--- subqart03/dist/src/models/user.model.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-const userModel = new mongoose.Schema({
-    username: { type: String },
-    firstname: { type: String },
-    lastname: { type: String }
-});
-export default mongoose.models.users || mongoose.model('user', userModel);
