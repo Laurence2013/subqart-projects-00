2f92219bca7d023ab3110b2ed0f57c6fbce0697c
Laurence2013 <lozza244@protonmail.com>
2025-02-25 09:14:20 +0000

    15-commit

--- subqart02/testings/test01.test.ts
+++ subqart02/testings/test01.test.ts
@@ -1,9 +1,5 @@
 import {test00} from '../src/index';
 
-/*test('adds 1 + 2 to equal 3', () => {
-  expect(test00(1, 2)).toBe(3);
-});*/
-
 describe('add function', () => {
   it('should add two numbers correctly', () => {
     expect(test00(1, 49)).toBe(50);
